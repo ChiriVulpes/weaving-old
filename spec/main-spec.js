f65bfc5380490fbdd2ee8acc8812bbf323d554af
Mackenzie McClane <aaritak@gmail.com>
2015-09-09 17:41:33 -0500

    Rewrite tests in Jasmine, add tests to npmignore, fix bugs

---
+++ spec/main-spec.js
@@ -0,0 +1,106 @@
+
+require("../weaving.js").proto();
+
+describe("weaving", function () {
+    describe("a normal string", function () {
+        it("should return what it is given", function () {
+            var str = "Hello, world!";
+            expect(str.weave()).toEqual(str);
+        });
+        it("should escape characters following a tilde", function () {
+            var tests = {
+                "~{~}": "{}",
+                "~\\~~ ~~~ ~~~~ ~~~~~": "\\~ ~ ~~ ~~~",
+                "~[~]": "[]"
+            };
+            for (var i in tests) {
+                expect(i.weave()).toEqual(tests[i]);
+            }
+        });
+    });
+    describe("with basic argument splicing", function () {
+        it("and one argument", function () {
+            expect("Hello, {0}!".weave("world")).toEqual("Hello, world!");
+        });
+        it("and two arguments", function () {
+            expect("Hello, {1}! My name is {0}.".weave("Joe", "world")).toEqual("Hello, world! My name is Joe.");
+        });
+    });
+    describe("with a conditional", function () {
+        var example = "Hello, world!{0? My name is {0}.}";
+        it("should include the conditional if it's truthy", function () {
+            expect(example.weave("Joe")).toEqual("Hello, world! My name is Joe.");
+        });
+        it("should not include the conditional if it's falsey", function () {
+            expect(example.weave()).toEqual("Hello, world!");
+        });
+    });
+    describe("with multiple arguments and a conditional", function () {
+        var example = "Hello, {1}!{0? My name is {0}.}";
+        it("should include the conditional if it's truthy", function () {
+            expect(example.weave("Joe", "world")).toEqual("Hello, world! My name is Joe.");
+        });
+        it("should not include the conditional if it's falsey", function () {
+            expect(example.weave(null, "world")).toEqual("Hello, world!");
+        });
+    });
+    describe("with an else conditional", function () {
+        var example = "{0?My name is {0}:I have no name}.";
+        it("should include the conditional if it's truthy", function () {
+            expect(example.weave("Joe")).toEqual("My name is Joe.");
+        });
+        it("should include the 'else' side of the conditional if it's falsey", function () {
+            expect(example.weave()).toEqual("I have no name.");
+        });
+    });
+    describe("with subkeys and object arguments", function () {
+        it("should access subkeys of arguments", function () {
+            var example = "Hello, {name}! You've been playing for {timePlayed} hours.";
+            var tests = [
+                {name: "ExampleUser", timePlayed: 234.3},
+                {name: "BobSteveJimJoeGuy", timePlayed: 15}
+            ];
+            for (var i = 0; i < tests.length; i++) {
+                expect(example.weave(tests[i]))
+                .toEqual("Hello, " + tests[i].name + "! You've been playing for " + tests[i].timePlayed + " hours.");
+            }
+        });
+        it("should be able to chain subkeys", function () {
+            var example = "Hello, {name}! You've been playing for {game.1.timePlayed} hours.";
+            var tests = [
+                {name: "ExampleUser2", game: [
+                    {timePlayed: 12},
+                    {timePlayed: 234.3}
+                ]},
+                {name: "BobSteveJimJoeGuy", game: [
+                    {timePlayed: 150},
+                    {timePlayed: 15}
+                ]}
+            ];
+            for (var i = 0; i < tests.length; i++) {
+                expect(example.weave(tests[i]))
+                .toEqual("Hello, " + tests[i].name + "! You've been playing for " + tests[i].game[1].timePlayed + " hours.");
+            }
+        });
+    });
+    describe("with looping through the argument list", function () {
+        it("should put them together with your given separator", function () {
+            var args = ["blah", "pie", "cake"];
+            expect(String.prototype.weave.apply("All arguments: {*}", args)).toEqual("All arguments: " + args.join(""));
+            expect(String.prototype.weave.apply("All arguments: {*, }", args)).toEqual("All arguments: " + args.join(", "));
+        });
+        it("should allow something custom for every entry in the array", function () {
+            var example = "{*, :Value {!}~: '{&}'}";
+            expect(example.weave("blah", "pie", "cake")).toEqual("Value 0: 'blah', Value 1: 'pie', Value 2: 'cake'");
+        });
+        it("should allow looping through subkeys rather than the actual argument list", function () {
+            var tests = [
+                {type: "users", list: ["Joe", "Bob", "Stevie"]},
+                {type: "fruits", list: ["pineapple", "melon", "kiwi"]}
+            ];
+            for (var i = 0; i < tests.length; i++) {
+                expect("List of {0}: {1*, }".weave(tests[i].type, tests[i].list)).toEqual("List of " + tests[i].type + ": " + tests[i].list.join(", "));
+            }
+        });
+    });
+});
--- weaving.js
+++ weaving.js
@@ -1,20 +1,42 @@
 
+Error.extend = function (name, message) {
+    return function () {
+        Error.captureStackTrace(this, this.constructor);
+        this.name = name;
+        Array.prototype.unshift.apply(arguments, [message]);
+        try {
+            this.message = weaving.weave.apply(weaving, arguments);
+        } catch (error) {
+            this.message = message;
+        }
+    };
+};
+
 var weave = function (str, args, strict) {
     this.str = str;
     this.cursor = -1;
     this.args = args;
+    this.keys = [];
+    this.vals = [];
     this.segments = [];
     this.result = "";
     this.strict = !!strict;
+    this.FormatError = Error.extend('FormatError', 'There was an error in your syntax, in the given string "{0}"');
+    this.ArgumentError = Error.extend('ArgumentError', 'There was an error using the argument identified by "{0}"');
+    this.UnsupportedError = Error.extend('UnsupportedError', 'Sorry, you used an currently unsupported feature: "{0}"');
 };
 weave.prototype.weave = function () {
-    this.extract();
-    this.result = this.compile(this.str)
-        .replace(/\.~\[/g, "~{")
-        .replace(/\.~\]/g, "~}")
-        .replace(/(~{2,})\[/g, function (match, escapes) {
-            return escapes.slice(2) + "[";
-        }).replace(/~(.)/g, "$1");
+    try {
+        this.extract();
+        this.result = this.compile(this.str)
+            .replace(/\.~\[/g, "~{")
+            .replace(/\.~\]/g, "~}")
+            .replace(/(~{2,})([\[\]])/g, function (match, escapes, escaped) {
+                return escapes.slice(2) + escaped;
+            }).replace(/~(.)/g, "$1");
+    } catch (error) {
+        throw error;
+    }
     return this.result;
 };
 weave.prototype.extract = function () {
@@ -37,24 +59,46 @@ weave.prototype.extract = function () {
     this.str = str;
     this.segments = segments;
 };
+var nextOccurence = function (rgx, str, offset) {
+    var occurence = rgx.exec(str.slice(offset));
+    return occurence && "index" in occurence ? occurence.index + occurence[0].length - 1 + offset : -1;
+};
 weave.prototype.compile = function (str) {
+    var error = new Error;
     if (weaving.tailsMatch(str, "{", "}")) {
-        var keys = this.keys(str.slice(1));
+        var keys = this.getKeys(str.slice(1));
         var offset = keys.offset + 1;
-        var value = this.getValue(keys.keys);
+        var value = keys.keys ? this.getValue(keys.keys) : undefined;
         if (str[offset] == "}") return value === undefined ? str : value;
         else if (str[offset] == "?") {
-            var colonOffset = /(^|[^~])(~~)*:/.exec(str.slice(offset));
-            colonOffset = colonOffset && "index" in colonOffset ? colonOffset.index + colonOffset[0].length - 1 + offset : -1;
+            var colonOffset = nextOccurence(/(^|[^~])(~~)*:/, str, offset);
             if (value) {
                 return this.compile(str.slice(offset + 1, colonOffset));
             } else {
-                return colonOffset == "-1" ? "" : this.compile(str.slice(colonOffset + 1, -1));
+                return colonOffset == -1 ? "" : this.compile(str.slice(colonOffset + 1, -1));
+            }
+        } else if (str[offset] == "*") {
+            if (value === undefined && keys.offset == 0) value = this.args;
+            if (typeof value == "object") {
+                var colonOffset = nextOccurence(/(^|[^~])(~~)*:/, str, offset);
+                var result = [];
+                var _this = this;
+                var add = function (i) {
+                    _this.keys.push(i);
+                    _this.vals.push(value[i]);
+                    result.push(_this.compile(colonOffset == -1 ? "{&}" : str.slice(colonOffset + 1, -1)));
+                    _this.keys.pop();
+                    _this.vals.pop();
+                }
+                if (value.constructor.name == "Array") for (var i = 0; i < value.length; i++) add(i);
+                else for (var i in value) add(i);
+                return result.join(this.compile(str.slice(offset + 1, colonOffset)));
             }
         } else {
-            if (this.strict) throw new Error;
-            else return value === undefined ? str : value;
+            if (this.strict) throw new this.FormatError(str);
         }
+        if (this.strict) throw new this.ArgumentError(keys.keys ? keys.keys.join('.') : 'unknown');
+        return value === undefined ? str : value;
     } else {
         var _this = this;
         return str.replace(/\[!(\d+)\]/g, function (match, index) {
@@ -62,8 +106,9 @@ weave.prototype.compile = function (str) {
             return result;
         });
     }
+    throw error;
 };
-weave.prototype.keys = function (str) {
+weave.prototype.getKeys = function (str) {
     var keys = [];
     var key = "";
     for (var i = 0; i < str.length && !/[?*:}]/.test(str[i]); i++) {
@@ -86,15 +131,27 @@ weave.prototype.escapeChar = function ( char ) {
     return char;
 };
 weave.prototype.getValue = function (keys) {
-    var number = keys[0].match(/^0|[1-9]\d*$/);
-    if (!number || number[0].length != keys[0].length) {
-        keys.unshift('0');
+    if (keys[0][0].match(/[&!]/)) {
+        if (keys[0].length == 1) {
+            keys[0] += "-1";
+        }
+        keys.unshift(keys[0][0]);
+        keys[1] = keys[1].slice(1);
+        if (!keys[1].match(/^0|-?[1-9]\d*$/)) return undefined;
+    } else {
+        var number = keys[0].match(/^0|[1-9]\d*$/);
+        if (!number || number[0].length != keys[0].length) {
+            keys.unshift('0');
+        }
     }
-    var val = this.args;
+    var val = keys[0] == "&" ? this.vals : keys[0] == "!" ? this.keys : this.args;
+    if (keys[0].match(/[&!]/)) keys.shift();
     for (var i = 0; i < keys.length; i++) {
-        if (typeof val != "object" || !keys[i] in val) {
-            console.log(val, keys);
-            return undefined;
+        if (typeof val != "object") return undefined;
+        else if (!(keys[i] in val)) {
+            if (keys[i].match(/-[1-9]\d*/)) keys[i] = val.length + Number(keys[i]);
+            else return undefined;
+            if (Math.sign(keys[i]) == -1) return undefined;
         }
         val = val[keys[i]];
     }
